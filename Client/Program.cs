39bf251c1dac1c0e9c368ba151e94e5e5467cfc7
Maksym Karapud <maks.karapud52@gmail.com>
2020-12-02 08:12:54 +0200

    Commit

---
+++ Client/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static int port = 8005;
+        static string address = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+
+                byte[] data;
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do
+                {
+                    data = new byte[256];
+                    bytes = socket.Receive(data, data.Length, 0); //Receive size of password
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                } while (socket.Available > 0);
+
+                int passSize = int.Parse(builder.ToString());
+                builder.Clear();
+
+                Console.WriteLine("Довжина пароля: " + passSize);
+                Console.Write("Введіть 1 якщо бажаєте спробувати вгадати пароль самостійно" +
+                              " або 2 для автоматичного підбору: ");
+                int.TryParse(Console.ReadLine(), out int answer);
+
+                if (answer == 1)
+                {
+                    socket.Send(Encoding.Unicode.GetBytes("user"));//Send type of guessing
+                    if (UserGuessing(socket))
+                        return;
+                }
+                else if (answer == 2)
+                {
+                    socket.Send(Encoding.Unicode.GetBytes("auto"));//Send type of guessing
+                    AutoGuessing(socket, passSize);
+                    do
+                    {
+                        data = new byte[256];
+                        bytes = socket.Receive(data, data.Length, 0); //Receive number of attempts
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (socket.Available > 0);
+                    
+                    Console.WriteLine("Кількість спроб: " + builder);
+                    builder.Clear();
+                }
+                else
+                {
+                    throw new Exception("Невідома команда");
+                }
+                
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void AutoGuessing(Socket socket, int size)
+        {
+            Console.Write("Введіть час (в секундах) скільки буде тривати підбір: ");
+            int.TryParse(Console.ReadLine(), out int sec);
+            //var finish = DateTime.Now.Ticks + (long)sec * 10000000;
+            socket.Send(Encoding.Unicode.GetBytes(sec.ToString()));
+            
+            for(long i = 0 ;/*DateTime.Now.Ticks < finish*/; i++)
+            {
+                string guess = Generate(i);
+                while (guess.Length < size)
+                {
+                    guess = "0" + guess;
+                }
+                socket.Send(Encoding.Unicode.GetBytes(guess));//Send auto guess
+                
+                Console.WriteLine($"Спроба {i+1}: " + guess);
+                var builder = new StringBuilder();
+                do
+                {
+                    var data = new byte[256];
+                    var bytes = socket.Receive(data, data.Length, 0); //Receive info about pass
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                } while (socket.Available > 0);
+                
+                if (builder.ToString() == "Yes")
+                {
+                    Console.WriteLine("Пароль вгадано!");
+                    return;
+                }
+                else if(builder.ToString() == "!")
+                {
+                    Console.WriteLine("Час вичерпано.");
+                    return;
+                }
+            }
+        }
+
+        static string Generate(long num)
+        {
+            string res = "";
+            
+            if (num == 0)
+            {
+                res = "0";
+            }
+            while (num > 0)
+            {
+                int i = (int)(num < 62 ? num : num % 62);
+                if (i < 10)
+                {
+                    res = i + res;
+                }
+                else if (i < 36)
+                {
+                    res = ((char)(i + 55)) + res;
+                }
+                else
+                {
+                    res = ((char)(i + 61)) + res;
+                }
+
+                num = num / 62;
+            }
+
+            
+
+            return res;
+        }
+        static bool UserGuessing(Socket socket)
+        {
+            for (int i = 1; i > 0; i++)
+            {
+                Console.Write($"Спроба {i}:");
+                string guess = Console.ReadLine();
+
+                socket.Send(Encoding.Unicode.GetBytes(guess)); //Send user guess
+
+                var builder = new StringBuilder();
+                do
+                {
+                    var data = new byte[256];
+                    var bytes = socket.Receive(data, data.Length, 0); //Receive info about pass
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                } while (socket.Available > 0);
+
+                if (builder.ToString() == "Yes")
+                {
+                    Console.WriteLine("Пароль вгадано!");
+                    return true;
+                }
+                else
+                    Console.WriteLine("Пароль не вгадано");
+
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static int port = 8005;
+        
+        static void Main(string[] args)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listenSocket.Bind(ipPoint);
+
+                listenSocket.Listen(10);
+
+                Console.Write("Задайте пароль для сервера (пароль має містити лише латиницю та цифри):");
+                string password = Console.ReadLine();
+                int n = password.Length;
+                
+                while (true)
+                {
+                    Console.WriteLine("Очікую на клієнта...");
+                    Socket handler = listenSocket.Accept();
+                    Console.WriteLine("Клієнт підключився до серверу.");
+                    
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    handler.Send(Encoding.Unicode.GetBytes(n.ToString()));//Send size of password
+                    
+                    do
+                    {
+                        data = new byte[256];
+                        bytes = handler.Receive(data);//Receive type of guessing
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    if (builder.ToString() == "user")
+                    {
+                        bool isGuessed = false;
+                        while (!isGuessed)
+                        {
+                            isGuessed = Guessing(handler, password);
+                        }
+                    }
+                    else
+                    {
+                        builder.Clear();
+                        do
+                        {
+                            data = new byte[256];
+                            bytes = handler.Receive(data);//Receive time
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        }
+                        while (handler.Available > 0);
+                        
+                        var finish = DateTime.Now.Ticks + (long) int.Parse(builder.ToString()) * 10000000;
+                        
+                        bool isGuessed = false;
+                        int count = 0;
+                        while (!isGuessed )
+                        {
+                            isGuessed = Guessing(handler, password);
+                            count++;
+                            if (DateTime.Now.Ticks > finish)
+                            {
+                                handler.Send(Encoding.Unicode.GetBytes("!"));
+                                count++;
+                                break;
+                            }
+                        }
+                        handler.Send(Encoding.Unicode.GetBytes(count.ToString()));//Send number of attempts
+                    }
+                    
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static bool Guessing(Socket handler, string password)
+        {
+            
+            var builder = new StringBuilder();
+                do
+                {
+                    var data = new byte[256];
+                    var bytes = handler.Receive(data);//Receive guess
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (handler.Available > 0);
+
+                if (builder.ToString() == password)
+                {
+                    handler.Send(Encoding.Unicode.GetBytes("Yes"));//Send info that pass is correct
+                    return true;
+                }
+                else if (builder.ToString() == "!")
+                {
+                    return true;
+                }
+                else
+                {
+                    handler.Send(Encoding.Unicode.GetBytes("No"));//Send info that pass is incorrect
+                    return false;
+                }
+        }
+    }
+}
